620d30b4aaf6f418e8c00158ac68938bb29c09bf
Lawe02 <lawza02@gmail.com>
2022-01-14 23:16:33 +0100

    can now save object persons in data.bin

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
 
 namespace Filhant1
 {
@@ -8,8 +12,9 @@ namespace Filhant1
         static void Main(string[] args)
         {
             List<person> persons = new List<person>();
-
+ 
            for ( int i = 0; i < 3; i++){
+                
                 Console.WriteLine("Skriv ner förnamn");
                 string Fnam = Console.ReadLine();
                 Console.WriteLine("Skriv ner efterenamn");
@@ -24,9 +29,26 @@ namespace Filhant1
 
             }
             Console.WriteLine("Tryck enter för att spara filen");
+            Console.ReadLine();
 
+            FileStream fs = new FileStream("data.bin", FileMode.Create);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(fs, persons);
+            }
+            catch(SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason " + e.Message);
+            }
+            finally 
+            {
+                fs.Close();
+            }
              
         }
+        [Serializable()]
         public class person
         {
             public string Fname { get; set; }
